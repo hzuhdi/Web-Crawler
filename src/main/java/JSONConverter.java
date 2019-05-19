0d2c0457d831a83556757d7962cc0f20f4149565
Omaima Alkharoosi <omaima94@iclould.com>
2019-05-19 18:31:52 +0200

    Declare JSONConverter class

---
+++ src/main/java/JSONConverter.java
@@ -0,0 +1,17 @@
+import models.Book;
+import models.Movie;
+import models.Music;
+import java.util.List;
+public class JSONConverter {
+
+    private  List <Music> musics;
+    private  List <Book> books;
+    private  List <Movie> movies;
+    private  Object objectMapper;
+
+    public  String toJson (Object object)
+    {
+        return  null;
+    }
+
+}
--- src/main/java/models/Movie.java
+++ src/main/java/models/Movie.java
@@ -1,5 +1,5 @@
 package models;
 
-public class Movies {
+public class Movie {
     // test movie class .........
 }
