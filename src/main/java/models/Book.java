94d695054d0b8dd9314ca196bb13a75e1d4aa9e3
Hafizh Ahmad Zuhdi <hafizhahmadzuhdi@gmail.com>
2019-05-19 17:08:46 +0200

    Add book model

---
+++ src/main/java/models/Book.java
@@ -0,0 +1,93 @@
+package models;
+
+import java.util.List;
+
+public class Book {
+    private int id;
+    private String title;
+    private String genre;
+    private String format;
+    private int year;
+    private List<String> authors;
+    private String publisher;
+    private String ISBN;
+
+
+    public Book() {
+    }
+
+    public Book(int id, String title, String genre, String format, int year, List<String> authors, String publisher, String ISBN) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.format = format;
+        this.year = year;
+        this.authors = authors;
+        this.publisher = publisher;
+        this.ISBN = ISBN;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+}
