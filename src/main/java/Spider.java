8c19cb1008cae787517d9c28ff58fb6ce1309bbf
Omaima Alkharoosi <omaima94@iclould.com>
2019-05-19 18:03:11 +0200

    Declare Spider class

--- src/main/java/Spider.java
+++ src/main/java/Spider.java
@@ -1,2 +1,14 @@
+import java.util.List;
+import java.util.Set;
 public class Spider {
+    private  int MAX_PAGES;
+    private  Set <String> pages_visitied ;
+    private  List<String> pages_to_visit  ;
+
+    public  String  nextUrl(){
+        return null;
+    }
+public  void search (String url, String keyword){
+
+}
 }
