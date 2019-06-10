fbadbcd13ef9fcf797970d037b1db10ba0ebe561
Al-Harrasi,Yousuf Y.A.S <y.alharrasi@student.fontys.nl>
2019-06-10 23:06:53 +0200

    Movie class added

--- src/main/java/models/Movie.java
+++ src/main/java/models/Movie.java
@@ -1,8 +1,38 @@
 package models;
 
-/**
- * TODO: Yousoef please fill this class
- */
+
+import java.util.List;
+
 public class Movie {
-    // test movie class .........
+    private int id;
+    private String title;
+    private String category = "Movies";
+    private String genre;
+    private String format;
+    private int year;
+    private String director;
+    private List<String> writers;
+    private List<String> stars;
+
+    /**
+     *
+     * @param id
+     * @param title
+     * @param genre
+     * @param format
+     * @param year
+     * @param director
+     * @param writers
+     * @param stars
+     */
+    public Movie(int id, String title, String genre, String format, int year, String director, List<String> writers, List<String> stars) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.format = format;
+        this.year = year;
+        this.director = director;
+        this.writers = writers;
+        this.stars = stars;
+    }
 }
