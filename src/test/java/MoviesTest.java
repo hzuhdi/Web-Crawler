5c7adbb47f3ef4a81525229d0dcfec9c0bf2ddbd
Al-Harrasi,Yousuf Y.A.S <y.alharrasi@student.fontys.nl>
2019-06-10 22:52:50 +0200

    titleShouldNotBeNull() is added - RED

--- src/test/java/MoviesTest.java
+++ src/test/java/MoviesTest.java
@@ -1,2 +1,28 @@
+
+import models.Movie;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class MoviesTest {
+
+    private Movie movie;
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void titleShouldNotBeNull() {
+        //Arrange
+        String title = "X Man";
+        //Act
+        movie=new Movie(title);
+        //Assert
+    }
+
 }
