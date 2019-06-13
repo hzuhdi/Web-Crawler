21b7c2f3151e3f944cbf16938e835e49e629d999
Hafizh Ahmad Zuhdi <hafizhahmadzuhdi@gmail.com>
2019-06-13 21:47:25 +0200

    Fix the git ignore and add a new Test for scraper - RED

--- src/test/java/ScraperTest.java
+++ src/test/java/ScraperTest.java
@@ -1,8 +1,13 @@
+import models.Book;
+import models.Movie;
+import models.Music;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.*;
+
+import java.util.ArrayList;
 
 import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -32,5 +37,28 @@ public class ScraperTest {
         scraper.setElements(document.getElementsByClass("media-details"));
         //Assert
         assertEquals(elements, scraper.getElements());
+    }
+
+    @Test
+    public void parseAllShouldReturnAListOfBooksMoviesAndMusics(){
+        //Arrange
+        Elements elements = new Elements();
+        Document document = mock(Document.class);
+        Scraper scraper = mock(Scraper.class);
+        ArrayList<Music> musics = new ArrayList<Music>();
+        ArrayList<Book> books = new ArrayList<Book>();
+        ArrayList<Movie> movies = new ArrayList<Movie>();
+        //Act
+        //Document.getElementsByClass will be called within the parseAll method
+        when(document.getElementsByClass("media-details")).thenReturn(elements);
+        scraper.parseAll(url);
+        movies = scraper.getMovies();
+        books = scraper.getBooks();
+        musics = scraper.getMusics();
+        //Assert
+        assertNotNull(movies);
+        assertNotNull(books);
+        assertNotNull(musics);
+
     }
 }
