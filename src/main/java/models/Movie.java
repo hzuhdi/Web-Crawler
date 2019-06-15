d35cf2401be7b75eda1c85ff69558ddb8eea0056
Al-Harrasi,Yousuf Y.A.S <y.alharrasi@student.fontys.nl>
2019-06-15 17:23:57 +0200

    1-MovieYearShouldBeLessThanOrEqualCurrentYearException class is added 2-Movie class -REFACTOR

--- src/main/java/models/Movie.java
+++ src/main/java/models/Movie.java
@@ -4,7 +4,9 @@ package models;
 import exception.*;
 
 import javax.naming.directory.AttributeInUseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Movie {
@@ -43,6 +45,16 @@ public class Movie {
         if (year <= 0) {
             throw new AttributeNotPresentedException("year should not be null or 0");
         }
+
+        Date currentDate = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy");
+        int currentYear = Integer.valueOf(formatter.format(currentDate));
+
+        if(year > currentYear)
+        {
+            throw new MovieYearShouldBeLessThanOrEqualCurrentYearException("Movie year should be less than or equal current");
+        }
+
         if (director == null) {
             throw new AttributeNotPresentedException("director should not be null or empty");
         }
@@ -93,6 +105,16 @@ public class Movie {
         if (year <= 0) {
             throw new AttributeNotPresentedException("year should not be null or 0");
         }
+
+        Date currentDate = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy");
+        int currentYear = Integer.valueOf(formatter.format(currentDate));
+
+        if(year > currentYear)
+        {
+            throw new MovieYearShouldBeLessThanOrEqualCurrentYearException("Movie year should be less than or equal current");
+        }
+
         if (director == null) {
             throw new AttributeNotPresentedException("director should not be null or empty");
         }
