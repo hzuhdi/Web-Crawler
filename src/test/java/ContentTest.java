384a1ce7fe045559ad60783dcfde601984a17f0b
Omaima Alkharoosi <omaima94@iclould.com>
2019-06-16 15:06:44 +0200

    Movie Year Should Be Equal or Lower Than Current Year

--- src/test/java/ContentTest.java
+++ src/test/java/ContentTest.java
@@ -1,10 +1,15 @@
+import exception.AllowedWritersNumberExceededException;
+import exception.AttributeNotPresentedException;
+import exception.MovieYearShouldBeLessThanOrEqualCurrentYearException;
 import exception.YearException;
 import models.Book;
+import models.Movie;
 import models.Music;
 import org.junit.Test;
 
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class ContentTest {
     @Test(expected = YearException.class)
@@ -16,4 +21,9 @@ public class ContentTest {
     public void YearOfMusicNotAfterTodayDate() throws YearException {
         Music music = new Music(12, "genre", "format", LocalDate.now().getYear() + 1, "artist", "title");
     }
+    @Test(expected = MovieYearShouldBeLessThanOrEqualCurrentYearException.class)
+    public void YearOfMovieNotAfterTodayDate() throws YearException, MovieYearShouldBeLessThanOrEqualCurrentYearException, AllowedWritersNumberExceededException, AttributeNotPresentedException {
+        Movie movie = new Movie(3, "title", "genre", "format", LocalDate.now().getYear() + 1, "director", Collections.emptyList(), Collections.emptyList());
+    }
+    
 }
