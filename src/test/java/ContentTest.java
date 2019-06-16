8c183f2dd6d4e36f08b0ed4c7609c20f6d5bde96
Omaima Alkharoosi <omaima94@iclould.com>
2019-06-16 15:03:21 +0200

    Music Year Should Be Equal or Lower Than Current Year

--- src/test/java/ContentTest.java
+++ src/test/java/ContentTest.java
@@ -1,5 +1,6 @@
 import exception.YearException;
 import models.Book;
+import models.Music;
 import org.junit.Test;
 
 import java.time.LocalDate;
@@ -9,5 +10,10 @@ public class ContentTest {
     @Test(expected = YearException.class)
     public void YearOfBookNotAfterTodayDate() throws YearException {
         Book book = new Book(2, "title", "genre", "formqt", LocalDate.now().getYear() + 1, Arrays.asList("author1"), "pib", "isbn");
+    }
+
+    @Test(expected = YearException.class)
+    public void YearOfMusicNotAfterTodayDate() throws YearException {
+        Music music = new Music(12, "genre", "format", LocalDate.now().getYear() + 1, "artist", "title");
     }
 }
