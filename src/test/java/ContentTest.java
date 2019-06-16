d6db7d46403f22862e0ef521fd7f1b3becada911
Omaima Alkharoosi <omaima94@iclould.com>
2019-06-16 14:48:41 +0200

    Book Year Should Be Equal or Lower Than Current Year

--- src/test/java/ContentTest.java
+++ src/test/java/ContentTest.java
@@ -1,2 +1,13 @@
+import exception.YearException;
+import models.Book;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
 public class ContentTest {
+    @Test(expected = YearException.class)
+    public void YearOfBookNotAfterTodayDate() throws YearException {
+        Book book = new Book(2, "title", "genre", "formqt", LocalDate.now().getYear() + 1, Arrays.asList("author1"), "pib", "isbn");
+    }
 }
