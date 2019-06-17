4436fed22880de5b299c4267a564cb025e8835a8
Al-Harrasi,Yousuf Y.A.S <y.alharrasi@student.fontys.nl>
2019-06-17 23:09:12 +0200

    getAllReturnValidResponse() is added -RED

--- src/test/java/WCA_ControllerTest.java
+++ src/test/java/WCA_ControllerTest.java
@@ -1,10 +1,31 @@
+import junit.framework.TestCase;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
 
 public class WCA_ControllerTest {
 
     @Before
     public void setUp() {
 
+    }
+
+    @Test
+    public void getAllReturnValidResponse()
+    {
+        // Arrange
+        WCA_Controller controller = new WCA_Controller();
+        Scraper scraper = new Scraper();
+        Crawler crawler = new Crawler();
+        Content content = new Content();
+        String url = "";
+
+        // Act
+        String response = controller.getAll(scraper, crawler, content, url);
+
+        // Assert
+        assertNotNull("Response is null", response);
     }
 
 
