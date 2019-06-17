96edc4d0a338efbc4defa7f15ee91ad3e9e36714
Al-Harrasi,Yousuf Y.A.S <y.alharrasi@student.fontys.nl>
2019-06-17 23:28:58 +0200

    getAll() is implemented in WCA_Controller class -REFACTOR

--- src/main/java/WCA_Controller.java
+++ src/main/java/WCA_Controller.java
@@ -1,8 +1,16 @@
+import exception.YearException;
+
+import java.util.Date;
+
 public class WCA_Controller {
 
     Scraper scraper;
     Crawler crawler;
     Content content;
+
+    private Date startTime;
+    private Date endTime;
+    private long timeElapsedInMS;
 
     public WCA_Controller(Scraper scraper, Crawler crawler, Content content) {
         this.scraper = scraper;
@@ -13,8 +21,18 @@ public class WCA_Controller {
     public WCA_Controller() {
     }
 
-    public String getAll(String url){
-        return "";
+    public String getAll(String url) throws YearException {
+        startTime = new Date();
+
+        scraper.parseAll(url);
+
+        endTime = new Date();
+
+        timeElapsedInMS = endTime.getTime() - startTime.getTime();
+
+        content.addToList(scraper.getBooks(), scraper.getMovies(), scraper.getMusics());
+
+        return content.converToJson();
     }
 
     public String getSpecific(String url, String keyword){
