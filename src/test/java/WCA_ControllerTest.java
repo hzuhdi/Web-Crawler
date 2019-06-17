0874154747cbf4b28785be6a929ce0bebefb7ddc
Al-Harrasi,Yousuf Y.A.S <y.alharrasi@student.fontys.nl>
2019-06-17 23:58:59 +0200

    getAllThrowsYearException() added -GREEN

--- src/test/java/WCA_ControllerTest.java
+++ src/test/java/WCA_ControllerTest.java
@@ -1,7 +1,11 @@
 import exception.YearException;
 import junit.framework.TestCase;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.time.Year;
 
 import static org.junit.Assert.*;
 
@@ -16,6 +20,10 @@ public class WCA_ControllerTest {
     public void setUp() {
 
     }
+
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
     @Test
     public void getAllReturnNonEmptyResponse() throws YearException
@@ -38,6 +46,25 @@ public class WCA_ControllerTest {
         // Assert
         assertNotNull("Response is null", response);
         assertFalse("Response is empty", response.trim().isEmpty());
+    }
+
+    @Test
+    public void getAllThrowsYearException() throws YearException
+    {
+        exception.expect(YearException.class);
+        exception.expectMessage(containsString("year"));
+
+        // Arrange
+        String url = "";
+        String jsonResponse = "Test";
+
+        WCA_Controller controller = mock(WCA_Controller.class);
+
+        // expectations
+        when(controller.getAll(anyString())).thenThrow(new YearException("Invalid year"));
+
+        // Act
+        String response = controller.getAll(url);
     }
 
 
