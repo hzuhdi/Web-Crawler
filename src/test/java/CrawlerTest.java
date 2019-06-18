b5dac5d5fc51dd52a9b4f290ed980808ebf16c70
Hafizh Ahmad Zuhdi <hafizhahmadzuhdi@gmail.com>
2019-06-18 22:10:14 +0200

    Fixing the crawler of returning a url size - RED

--- src/test/java/CrawlerTest.java
+++ src/test/java/CrawlerTest.java
@@ -2,10 +2,14 @@ import org.junit.Test;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertEquals;
+
 public class CrawlerTest {
     @Test
     public void getURLSizeReturnIntValue() throws IOException {
         Crawler crawler = new Crawler();
+        crawler.loopUrl("http://localhost/sample_site_to_crawl/catalog.php");
+        assertEquals(12, crawler.getUrlSize());
     }
     @Test(expected = IllegalArgumentException.class)
     public void parameterOfLoopUrlShouldNotBeNull() throws IOException {
