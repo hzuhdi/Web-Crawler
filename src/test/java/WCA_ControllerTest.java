474f303acbd2c40ac7ae6110ec42519a0760db30
Al-Harrasi,Yousuf Y.A.S <y.alharrasi@student.fontys.nl>
2019-06-19 00:58:36 +0200

    getAllReturnValidResponse() -GREEN

--- src/test/java/WCA_ControllerTest.java
+++ src/test/java/WCA_ControllerTest.java
@@ -1,17 +1,24 @@
+import com.google.gson.Gson;
+import exception.AllowedWritersNumberExceededException;
+import exception.AttributeNotPresentedException;
+import exception.MovieYearShouldBeLessThanOrEqualCurrentYearException;
 import exception.YearException;
 import junit.framework.TestCase;
 import models.Book;
 import models.Movie;
 import models.Music;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.Year;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -33,8 +40,7 @@ public class WCA_ControllerTest {
     public ExpectedException exception = ExpectedException.none();
 
     @Test
-    public void getAllReturnNonEmptyResponse() throws YearException, IOException
-    {
+    public void getAllReturnNonEmptyResponse() throws YearException, IOException {
         // Arrange
         Scraper scraper = mock(Scraper.class);
         Crawler crawler = mock(Crawler.class);
@@ -56,8 +62,7 @@ public class WCA_ControllerTest {
     }
 
     @Test
-    public void getAllThrowsYearException() throws YearException, IOException
-    {
+    public void getAllThrowsYearException() throws YearException, IOException {
         exception.expect(YearException.class);
         exception.expectMessage(containsString("year"));
 
@@ -75,56 +80,12 @@ public class WCA_ControllerTest {
     }
 
     @Test
-    public void getAllReturnValidResponse() throws YearException, IOException
-    {
+    public void getAllReturnValidResponse() throws YearException, IOException {
         // Arrange
-        Scraper scraper = mock(Scraper.class);
-        Crawler crawler = mock(Crawler.class);
-        Content content = mock(Content.class);
-        String url = "";
-        String jsonResponse = "{\n" +
-                "   \"id\": 3,\n" +
-                "   \"time\":1499696751,\n" +
-                "   \"movies\":[\n" +
-                "      {\n" +
-                "         \"name\":\"Office Space\",\n" +
-                "         \"genre\":\"Comedy\",\n" +
-                "         \"format\":\"Blu-ray\",\n" +
-                "         \"year\":1999,\n" +
-                "         \"director\":\"Mike Judge\",\n" +
-                "         \"writers\":[\n" +
-                "            \"William Goldman\"\n" +
-                "         ],\n" +
-                "         \"stars\":[\n" +
-                "            \"Ron Livingston\",\n" +
-                "            \"Jennifer Aniston\",\n" +
-                "            \"David Herman\",\n" +
-                "            \"Ajay Naidu\",\n" +
-                "            \"Diedrich Bader\",\n" +
-                "            \"Stephen Root\"\n" +
-                "         ]\n" +
-                "      }\n" +
-                "   ],\n" +
-                "   \"books\":[\n" +
-                "      {\n" +
-                "         \"name\":\"The Clean Coder: A Code of Conduct for Professional Programmers\",\n" +
-                "         \"genre\":\"Tech\",\n" +
-                "         \"format\":\"Audio\",\n" +
-                "         \"year\":2011,\n" +
-                "         \"authors\":\"Robert C.Martin\",\n" +
-                "         \"publisher\":\"Prentice Hall\",\n" +
-                "         \"ISBN\":\"007-6092046981\"\n" +
-                "      }\n" +
-                "   ],\n" +
-                "   \"music\":[\n" +
-                "      {\n" +
-                "         \"name\":\"Beethoven: Complete Symphonies\",\n" +
-                "         \"format\":\"CD\",\n" +
-                "         \"year\":2012,\n" +
-                "         \"artist\":\"Ludwig van Beethoven\"\n" +
-                "      }\n" +
-                "   ]\n" +
-                "}\n";
+        Scraper scraper = new Scraper();
+        Crawler crawler = new Crawler();
+        Content content = new Content();
+        String url = "http://localhost/sample_site_to_crawl/details.php?id=204";
 
         ArrayList<Book> books = new ArrayList<>();
         ArrayList<Movie> movies = new ArrayList<>();
@@ -137,20 +98,15 @@ public class WCA_ControllerTest {
         List<String> authors = new ArrayList<>(Arrays.asList("author 1", "author 2"));
 
         books.add(new Book(1, "How to code in Java", "Computer", "pdf", 2009, authors, "Gramedia Publisher", "ISBN123456789"));
-        movies.add(new Movie(1, "Office Space", "Drama", "Blue-ray",2001,"Peter Jackson" , writers, stars));
+        movies.add(new Movie(1, "The Princess Bride", "Drama", "Blue-ray", 2001, "Peter Jackson", writers, stars));
         musics.add(new Music(1, "genre1", "format", 2011, ("artist1"), "title"));
 
-        // expectations
-        //TODO, remove the followng content exectation, when content.convertToJson is implemented
-        when(content.converToJson()).thenReturn(jsonResponse);
-
-        when(scraper.getBooks()).thenReturn(books);
-        when(scraper.getMovies()).thenReturn(movies);
-        when(scraper.getMusics()).thenReturn(musics);
+        content.setBooks(books);
+        content.setMovies(movies);
+        content.setMusics(musics);
 
         // Act
         String response = controller.getAll(url);
-
         System.out.println(response);
 
         // Assert
@@ -159,10 +115,7 @@ public class WCA_ControllerTest {
         assertTrue(response.contains("movies"));
         assertTrue(response.contains("books"));
         assertTrue(response.contains("music"));
-
-        //assertTrue(response.contains("J.R.R. Tolkien"));
+        assertTrue(response.contains("The Princess Bride"));
     }
-
-
 
 }
