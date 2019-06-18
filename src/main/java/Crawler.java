e4f910bf5bed83ebb3a3983bde92289481a3f526
Omaima Alkharoosi <omaima94@iclould.com>
2019-06-18 09:12:52 +0200

    Adding URL_PATTERN property to Crawler class

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -6,6 +6,7 @@ public class Crawler {
     private int MAX_PAGES;
     private Set<String> pages_visited;
     private ArrayList<String> pages_to_visit;
+    private static final String URL_PATTERN = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
 
     public Crawler(){
 
