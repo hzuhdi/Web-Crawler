75b6b6c2b9db089d4b1d03cf3150aa3d53ce6c84
Hafizh Ahmad Zuhdi <hafizhahmadzuhdi@gmail.com>
2019-06-18 13:20:42 +0200

    Initialize the UI

--- src/main/java/WCA_Main.java
+++ src/main/java/WCA_Main.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class WCA_Main {
 
     WCA_Controller wca_controller = new WCA_Controller();
 
     public static void main(String agrs[]){
+
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("------Welcome to Web Crawler Application TCI ------");
+            System.out.println("--Make sure that your local web server is running--");
+            System.out.println("---------------------------------------------------");
+            System.out.println("Please choose a number");
+            System.out.println("1. Crawl All");
+            System.out.println("2. Crawl Specific");
+            System.out.println("3. Exit");
+            int x = sc.nextInt();
+            if(x==3){
+                break;
+            }
+        }
+
 
     }
 }
