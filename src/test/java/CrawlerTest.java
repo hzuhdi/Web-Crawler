36a24612575c4d1448ebf5c6931b0a2078f5a515
Omaima Alkharoosi <omaima94@iclould.com>
2019-06-18 09:07:06 +0200

    Get URL Size Return Int Value

--- src/test/java/CrawlerTest.java
+++ src/test/java/CrawlerTest.java
@@ -1,2 +1,11 @@
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class CrawlerTest {
+    @Test
+    public void getURLSizeReturnIntValue() throws IOException {
+        Crawler crawler = new Crawler();
+    }
+
 }
