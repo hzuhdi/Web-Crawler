23ccd06b9a8aff6549fe48c9a3d84fa8b619c7f4
Omaima Alkharoosi <omaima94@iclould.com>
2019-06-18 15:00:55 +0200

    implement crawl method

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -69,7 +69,30 @@ class CrawlerUtil {
     private Document htmlDocument;
     private static final String USER_AGENT ="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
 
-    public boolean crawl(String url) {return true;}
+    public boolean crawl(String url) {
+        try {
+            Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
+            Document htmlDocument = connection.get();
+            this.htmlDocument = htmlDocument;
+            if (connection.response().statusCode() == 200) {
+                System.out.println("\n**Visiting** Received web page at " + url);
+            }
+            if (!connection.response().contentType().contains("text/html")) {
+                System.out.println("**Failure** Retrieved something other than HTML");
+                return false;
+            }
+            Elements linksOnPage = htmlDocument.select("a[href]");
+            System.out.println("Found (" + linksOnPage.size() + ") links");
+            for (Element link : linksOnPage) {
+                this.links.add(link.absUrl("href"));
+            }
+            return true;
+        } catch (IOException ioe) {
+            // We were not successful in our HTTP request
+            return false;
+        }
+
+    }
     public boolean searchForWord(String searchWord){return true;}
     public List<String> getLinks() {
         return links;
