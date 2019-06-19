5a10e1221fef5d98233326f3b03270da0a6bc71c
Hafizh Ahmad Zuhdi <hafizhahmadzuhdi@gmail.com>
2019-06-19 07:59:12 +0200

    Add a main class

--- src/main/java/WCA_Main.java
+++ src/main/java/WCA_Main.java
@@ -1,10 +1,13 @@
+import exception.YearException;
+
+import java.io.IOException;
 import java.util.Scanner;
 
 public class WCA_Main {
 
     WCA_Controller wca_controller = new WCA_Controller();
 
-    public static void main(String agrs[]){
+    public static void main(String agrs[]) throws YearException, IOException {
 
         Scanner sc = new Scanner(System.in);
         while(true){
@@ -16,7 +19,23 @@ public class WCA_Main {
             System.out.println("2. Crawl Specific");
             System.out.println("3. Exit");
             int x = sc.nextInt();
-            if(x==3){
+            if(x==1){
+                WCA_Controller wca_controller = new WCA_Controller();
+                System.out.println("Please input the url you want to take a look for: e.g: http://localhost/sample_site_to_crawl/index.php" );
+                String input = sc.nextLine();
+                String xy = wca_controller.getAll(input);
+                System.out.println(xy);
+            } else if(x==2){
+                WCA_Controller wca_controller = new WCA_Controller();
+                System.out.println("Please input the url you want to take a look for: e.g: http://localhost/sample_site_to_crawl/index.php" );
+                String input = sc.nextLine();
+                System.out.println("Please input the keyword" );
+                String word = sc.nextLine();
+                String xy = wca_controller.getSpecific(input, word);
+                System.out.println(xy);
+            } if(x==3){
+                break;
+            } else {
                 break;
             }
         }
