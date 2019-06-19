9bb39ed36ede2ea1e9ab8d38bf861509e8a06e57
Hafizh Ahmad Zuhdi <hafizhahmadzuhdi@gmail.com>
2019-06-19 17:41:54 +0200

    shouldGetAllUrl - RED

--- src/test/java/CrawlerTest.java
+++ src/test/java/CrawlerTest.java
@@ -10,16 +10,22 @@ public class CrawlerTest {
     @Test(expected = IllegalArgumentException.class)
     public void parameterOfLoopUrlShouldNotBeNull() throws IOException {
         Crawler crawler = new Crawler();
-        crawler.loopUrl(null, null);
+        //crawler.loopUrl(null, null);
     }
     @Test(expected = IOException.class)
     public void loopUrlInvalid() throws IOException {
         Crawler crawler = new Crawler();
-        crawler.loopUrl("Invalid Url here", "demoWord");
+        //crawler.loopUrl("Invalid Url here", "demoWord");
     }
     /*@Test(expected = NullPointerException.class)
     public void pageToVisitIsNotEmpty() throws IOException {
         Crawler crawler = new Crawler();
         crawler.loopUrl("https://www.youtube.com/","demoWorld");
     }*/
+
+    @Test
+    public void shouldGetAllUrl() throws IOException {
+        Crawler crawler = new Crawler();
+        crawler.getAllUrl("http://localhost/sample_site_to_crawl/catalog.php");
+    }
 }
